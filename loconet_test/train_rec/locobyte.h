0f28143c32627240e2e51ddd50f6bdf69310d18a
Christopher Bero <bigbero@gmail.com>
2015-02-22 20:28:10 -0600

    cleaned locobyte.h from old method declarations

--- loconet_test/train_rec/locobyte.h
+++ loconet_test/train_rec/locobyte.h
@@ -23,7 +23,6 @@ public:
     bool get_isOP();
     short unsigned int get_packetLength();
     bool get_followOnMsg();
-    //char get_nybleAsChar(int _nyble);
     QBitArray get_qBitArray();
     void set_fromBinary(QString _binary);
     void set_fromHex(QString _hex);
@@ -35,17 +34,10 @@ public:
 
 protected:
     void bitsFromHex(QString _hex, int _nyble);
-    //void hexFromBits();
-    //void binaryFromBits();
-    //void bitsFromBinary();
     void createEmpty();
 
 private:
-    //bool ** byte; // Divided into nyble arrays
     QBitArray byte;
-    //QChar byteCharArray[2];
-    //QString binary; // Eight 0/1 characters representing binary
-    //bool OPcode; // Represents whether the hex is an OPcode or not
     static bool debug; // Set in main cpp
 };
 
